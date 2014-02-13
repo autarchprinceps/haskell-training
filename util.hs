9737f452d43d5c005678aaf8d59f57456c4d9437
autarch princeps <autarch@outlook.com>
2014-02-13 12:28:06 +0100

    Some Stuff

--- util.hs
+++ util.hs
@@ -1,4 +1,13 @@
-mapcomb func l m = [func a b | a <- l, b <- m]
+mapcomb func l m [] [] = [func a b | a <- l, b <- m]
+mapcomb func l m p1 p2 = [func a b p1 p2 | a <- l, b <- m]
 
-mapsingle _ _ [] = []
-mapsingle func x (l:ls) = func x l : mapsingle func x ls
+mapsingle _ _ [] _ _ = []
+mapsingle func x (l:ls) [] [] = func x l : mapsingle func x ls [] []
+mapsingle func x (l:ls) p1 p2 = func x l p1 p2 : mapsingle func x ls p1 p2
+
+keyvaluetester key l
+	| fst l == key = snd l
+	| otherwise = []
+
+keyvalue [] _ = []
+keyvalue (l:ls) x = if fst l == x then snd l else keyvalue ls x
